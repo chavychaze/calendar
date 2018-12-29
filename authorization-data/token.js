65d896c3bba8822831e00f77e4c772f660f04225
sobolv <vitaliy.sobol@startupsoft.us>
2018-12-29 17:33:52 +0200

    Get rid of read/write streams, the limit of GoogleAPI requests finished

---
+++ authorization-data/token.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "access_token": "ya29.GltxBgF9lqqYq4I9y-_x5z9jFYHUx4eEbZBNc2Ord0JgDz1Vgx3wSuxVEmr7HuUuJvfJg2Gn-H7FkXyNdZmUoBdC683iHS42lzd8C4E43Hj5E9kcAtPOoqZubLZf",
+    "refresh_token": "1/msBA0imb_at0VGcGSRca0_FwXtoKNYqkD4ppQvG7m3LnjXMKuZFP0nvTVfP6M7Dx",
+    "scope": "https://www.googleapis.com/auth/calendar.readonly",
+    "token_type": "Bearer",
+    "expiry_date": 1544722575078
+}
--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
+const express = require('express');
+
+var credentials = require('./authorization-data/credentials').installed;
+var token = require('./authorization-data/token');
+
+const app = express();
+
+const port = 5000;
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json';
-
-// Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), listEvents);
-});
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -22,17 +20,17 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+const authorize = (credentials) => {
+  const client_secret = credentials["client_secret"]
+    , client_id = credentials["client_id"]
+    , redirect_uris = credentials["redirect_uris[0]"];
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id, client_secret, redirect_uris);
+  if (!token) return getAccessToken(oAuth2Client, callback);
+
+  oAuth2Client.setCredentials(token);
+  // return callback(oAuth2Client);
 }
 
 /**
@@ -41,28 +39,16 @@ function authorize(credentials, callback) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getAccessToken(oAuth2Client, callback) {
+const getAccessToken = (oAuth2Client, callback) => {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
   });
   console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
+  oAuth2Client.getToken(code, (err, token) => {
+    if (err) return console.error('Error retrieving access token', err);
+    oAuth2Client.setCredentials(token);
+    return callback(oAuth2Client);
   });
 }
 
@@ -70,8 +56,9 @@ function getAccessToken(oAuth2Client, callback) {
  * Lists the next 10 events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
+const listEvents = () => {
+  const auth = authorize(credentials);
+  const calendar = google.calendar({ version: 'v3', auth });
   calendar.events.list({
     calendarId: 'primary',
     timeMin: (new Date()).toISOString(),
@@ -80,15 +67,29 @@ function listEvents(auth) {
     orderBy: 'startTime',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
+    // const events = res.data.items;
+    return res.data.items;
+    // if (events.length) {
+    // console.log('Upcoming 10 events:');
+    //   return events.map((event, i) => {
+    //     const start = event.start.dateTime || event.start.date;
+    //     console.log(`${start} - ${event.summary}`);
+    //   });
+    // } else {
+    //   console.log('No upcoming events found.');
+    // }
   });
 }
+// authorize(credentials, listEvents);
+
+app.get('/api/calendar', (req, res) => {
+  const data = listEvents();
+  if(!data) res.json('Error');
+  const start = data.start.dateTime || data.start.date;
+  console.log(`${start} - ${data.summary}`);
+  // console.log('aaaaaaaaaaaa', data)
+  res.json(`${start} - ${data.summary}`);
+});
+// console.log('aaaaaaaaaaaaaaaaaaaa', authorize(credentials, listEvents))
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
