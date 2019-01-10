f7d56b8d6b1813b106b9ab08cda1fef6574ee101
sobolv <vitaliy.sobol@startupsoft.us>
2019-01-10 19:10:09 +0200

    finish withfetching data

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import Calendar from './components/calendar/calendar';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <Calendar />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Calendar />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/calendar/calendar.js
+++ client/src/components/calendar/calendar.js
@@ -1,39 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './calendar.css';
 
-class Calendar extends Component {
-    constructor() {
-        super();
-        this.state = {
-            calendar: []
-        }
-    }
-
-    componentDidMount() {
+function Calendar() {
+    const [calendar, setState] = useState([]);
+    useEffect(() => {
         fetch('/api/calendar', {
-            headers : { 
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-             }
-      
-          })
-            .then(res => res.json())
-            // .then(text => console.log(text));
-            .then(calendar => this.setState({ calendar }, () => console.log('Calendar fetched...', calendar)));
-    }
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
 
-    render() {
-        return (
-            <div>
-                <h2>Calendar</h2>
-                <ul>
-                    {this.state.calendar.map(calendar => 
-                        <li key={calendar.id}>{calendar.start.dateTime} {calendar.summary}</li>
-                        )}
-                </ul>
-            </div>
-        );
-    }
+        })
+            .then(res => res.json())
+            .then(calendar => setState(calendar));
+    }, []);
+
+    return (
+        <div>
+            <h2>Calendar</h2>
+            <ul>
+                {calendar.map(calendar =>
+                    <li key={calendar.id}>{calendar.start.dateTime} {calendar.summary}</li>
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default Calendar;
